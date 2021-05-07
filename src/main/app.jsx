bac3bfb3b0b95da766072cf4d5be406493ff5682
AlbertoParente <albertoparente20@gmail.com>
2021-05-06 22:47:33 -0300

    Adding index.jsx and adjusting

---
+++ src/main/app.jsx
@@ -0,0 +1,9 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'mudules/font-awesome/css/font-awesome.min.css'
+import React from 'react'
+
+export default props => (
+    <div className="container">
+        <h1>Test</h1>
+    </div>
+)
