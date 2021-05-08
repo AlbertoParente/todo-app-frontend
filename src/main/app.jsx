9dadca2650039aa992e3085e6a787cf71ed81952
AlbertoParente <albertoparente20@gmail.com>
2021-05-07 23:37:54 -0300

    Creating the component Todo and About

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,9 +1,12 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'mudules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 export default props => (
     <div className="container">
-        <h1>Test</h1>
+        <Todo />
+        <About />
     </div>
 )
---
+++ src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Components {
+    render() {
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
