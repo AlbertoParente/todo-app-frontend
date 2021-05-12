1b3be082f5e6b2c0dbf08fbce91e9f62479b43ee
AlbertoParente <albertoparente20@gmail.com>
2021-05-11 23:30:17 -0300

    Adding If component

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import If from './if'
 
-export default props => {
-    if (props.hide) {
-        return null
-    } else {
-        return (
-            <button className={'btn btn-' + props.style}
-                onclick={props.onclick}>
-                <i className={'fa fa-' + props.icon}></i>
-            </button>
-        )
-    }
-}
+export default props => (
+    <If test={!props.hide}>
+        <button className={'btn btn-' + props.style}
+            onclick={props.onclick}>
+            <i className={'fa fa-' + props.icon}></i>
+        </button>
+    </If>
+)
