debb6bcd095000b9cf3cabb362670857fbcc6369
AlbertoParente <albertoparente20@gmail.com>
2021-05-17 22:47:51 -0300

    Adding shortcut keys

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -2,22 +2,33 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role="form" className="todoForm">
+export default props => {
+    const keyHandle = (e) => {
+        if(e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Espace') {
+            props.headleClear()
+        }
+    }
+    
+    return (
+        <div role="form" className="todoForm">
 
-        <Grid clos="12 9 10">
-            <input id="description" className="form-control" 
-                placeholder="Add a task" 
-                onChange={props.handleChange}
-                value={props.description}></input>
-        </Grid>
-        <Grid clos="12 3 2">
-            <IconButton style="primary" icon='plus' 
-                onClick={props.handleAdd} />
-            <IconButton style="info" icon="search"
-                onClick={props.handleSearch} />
-            <IconButton style="default" icon="close"
-                onClick={props.headleClear} />
-        </Grid>
-    </div>
-)
+            <Grid clos="12 9 10">
+                <input id="description" className="form-control"
+                    placeholder="Add a task"
+                    onChange={props.handleChange}
+                    onKeyUp={KeyHandle}
+                    value={props.description}></input>
+            </Grid>
+            <Grid clos="12 3 2">
+                <IconButton style="primary" icon='plus'
+                    onClick={props.handleAdd} />
+                <IconButton style="info" icon="search"
+                    onClick={props.handleSearch} />
+                <IconButton style="default" icon="close"
+                    onClick={props.headleClear} />
+            </Grid>
+        </div>
+    )
+}
