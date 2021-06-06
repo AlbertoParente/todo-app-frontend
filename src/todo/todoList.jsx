7d9d746bb62cab2f8820b9c7c2aee07368747ea6
AlbertoParente <albertoparente20@gmail.com>
2021-06-05 22:32:55 -0300

    Refactoring code for implementation with redux. Connecting TodoList and TodoForm

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import IconButton from '../template/iconButton'
 
-export default props => {
+const TodoList = props => {
     const renderRows = () => {
         const list = props.list || []
         return list.map(todo => (
@@ -33,3 +34,7 @@ export default props => {
         </table>
     )
 }
+
+const mapStateToProps = state => ({ list: state.todo.list })
+
+export default connect(mapStateToProps)(TodoList)
