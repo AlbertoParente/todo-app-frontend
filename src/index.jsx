7c0f1ba9618b5e267751c7bbcb0af8b1959bb035
AlbertoParente <albertoparente20@gmail.com>
2021-06-07 23:21:12 -0300

    Adding Action Creator, continuation

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,8 @@ import { Provider } from 'react-redux'
 import App from './main/app'
 import Reducers from '../src/main/reducers'
 
-const store = createStore(Reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(Reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -2,3 +2,11 @@ export const changesDecription = event => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
