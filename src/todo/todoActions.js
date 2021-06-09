6626fe834fc8a296823bf137562c82e2b60d10b6
AlbertoParente <albertoparente20@gmail.com>
2021-06-08 23:57:18 -0300

    Adding test

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -10,3 +10,46 @@ export const search = () => {
         payload: request
     }
 }
+
+
+export const changesDecription = event => ({
+    type: 'DESCRIPTION_CHANGED_TEST01',
+    payload: event.target.value
+})
+
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
+
+
+
+export const changesDecription = event => ({
+    type: 'DESCRIPTION_CHANGED_TESTE02',
+    payload: event.target.value
+})
+
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
+
+
+export const changesDecription = event => ({
+    type: 'DESCRIPTION_CHANGED_TESTE03',
+    payload: event.target.value
+})
+
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
